a58d6b1e5d08646abb296f6fa860b6e9d01886fe
nmarsollier@gmail.com <nmarsollier@gmail.com>
2018-03-20 14:05:14 -0300

    Format

--- javascript/assets/js/javascript.js
+++ javascript/assets/js/javascript.js
@@ -1,8 +1,7 @@
-
 // Las variables se deben definir utilizando la palabra var :
 console.log("Variables");
 
-      var cadena1 = "Primer cadena de caracteres";
+var cadena1 = "Primer cadena de caracteres";
 var cadena2 = "Cadena compuesta con 'comillas simples'";
 var cadena3 = 'Cadena con "comillas dobles" y caracteres unicode \u0041';
 var cadenaConcatenada = 'Cadena ' + "concatenada";
@@ -71,12 +70,12 @@ isNaN("12"); // false "12" se evalua como numero
 //Arrays
 console.log("\n\n\nArrays");
 
-var dias = [ "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" ]; // Arreglo de caracteres.
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]; // Arreglo de caracteres.
 var primerElemento = dias[0]; // = "Lunes"
 
 var numeroDias = dias.length; // = 7
 
-var array = [ "hola", "mundo" ];
+var array = ["hola", "mundo"];
 var mensaje = array.join(""); // mensaje = "holamundo"
 mensaje = array.join(" "); // mensaje = "hola mundo"
 
@@ -84,13 +83,13 @@ var ultimo = array.pop(); // ahora array = ["hola"], ultimo = "mundo"
 
 array.push("mundo"); // array = ["hola", "mundo"]
 
-array = [ 1, 2, 3 ]; // Una reasignacion puede cambiar el tipo de datos del array
+array = [1, 2, 3]; // Una reasignacion puede cambiar el tipo de datos del array
 var primero = array.shift(); // ahora array = [2, 3], primero = 1
 
 
-array = [ 1, "hola", {
-	text : "mundo"
-} ]; // Podemos incluir cualquier elemento en el array  
+array = [1, "hola", {
+    text: "mundo"
+}]; // Podemos incluir cualquier elemento en el array  
 
 
 
@@ -132,35 +131,33 @@ var porcion = mensaje.substring(1, 8); // porcion = "ola Mun"
 console.log("\n\n\n Funciones");
 
 function sumaMuestra(numero1, numero2) {
-	var resultado = numero1 + numero2;
-	console.log("El resultado es " + resultado);
-}
-;
+    var resultado = numero1 + numero2;
+    console.log("El resultado es " + resultado);
+};
 
 sumaMuestra(1, 3); // llamada a la funcion
 
 var sumaMuestra1 = function(numero1, numero2) { // Un puntero a funcion.
-	var resultado = numero1 + numero2;
-	return "El resultado es " + resultado; // En este ejemplo la funcion retorna un valor.
+    var resultado = numero1 + numero2;
+    return "El resultado es " + resultado; // En este ejemplo la funcion retorna un valor.
 };
 console.log(sumaMuestra1(1, 3)); // LLamada con punteros.
 
 
 function accesoGlobal() {
-	console.log("Accedemos a la variable global mensaje " + mensaje);
-}
-;
+    console.log("Accedemos a la variable global mensaje " + mensaje);
+};
 
 accesoGlobal();
 
 // Funciones anonimas
 
 (function() {
-	console.log("Funcion anonima e inmediata");
+    console.log("Funcion anonima e inmediata");
 })();
 
 ((param) => {
-	console.log("Arrow function anonimo " + param);
+    console.log("Arrow function anonimo " + param);
 })("el parametro");
 
 
@@ -200,10 +197,10 @@ console.log("'false' && true : " + ('false' && true)); // true 'false' evalua co
 // Para darle sentido al uso de booleans creamos nuestra propia funcion de parseo de boolean
 // Sin embargo es conveniente usar alguna libreria de funciones como underscore
 function parseBoolean(string) {
-	booleanValue = String(string).toLowerCase();
-	return booleanValue === 'true' ||
-		booleanValue === 'yes' ||
-		booleanValue === '1';
+    booleanValue = String(string).toLowerCase();
+    return booleanValue === 'true' ||
+        booleanValue === 'yes' ||
+        booleanValue === '1';
 }
 
 console.log("parseBoolean('false') = " + (parseBoolean('false')));
@@ -211,9 +208,9 @@ console.log("parseBoolean('false') = " + (parseBoolean('false')));
 // Arrays
 console.log("\n\n\n Arrays");
 
-var vocales = [ "a", "e", "i", "o", "u" ];
+var vocales = ["a", "e", "i", "o", "u"];
 console.log("vocales.length = " + (vocales.length)); // numeroVocales = 5
-var array1 = [ 1, 2, 3 ];
+var array1 = [1, 2, 3];
 array2 = array1.concat(4, 5, 6);
 console.log("array2 = " + (array2)); //array2 = [1, 2, 3, 4, 5, 6]
 
@@ -231,64 +228,64 @@ console.log("array2 = " + (array2)); //array2 = [1, 2, 3, 4, 5, 6]
 */
 console.log("\n\n\n Condicionales");
 if (numerico) {
-	console.log("ok numerico == 12 ==> true"); // Numerico tiene valor, ingresa
+    console.log("ok numerico == 12 ==> true"); // Numerico tiene valor, ingresa
 }
 if (!NaN) {
-	console.log("ok !NaN es true, NaN es false"); // NaN evalua a falso ? 
+    console.log("ok !NaN es true, NaN es false"); // NaN evalua a falso ? 
 }
 if (!null) {
-	console.log("ok !null es true, null es false"); // null evalua a falso
+    console.log("ok !null es true, null es false"); // null evalua a falso
 }
 if (numerico > 3) {
-	console.log("ok"); // Numerico tiene valor > 3
+    console.log("ok"); // Numerico tiene valor > 3
 }
 console.log(12 + 2 == 14 ? "ok 12 + 2 == 14 " : "no"); // La operacion matematica da 14
 
 
 console.log("Condicional- case ");
 switch (numerico) {
-case 12:
-	console.log("Es 12");
-	break;
-default:
-	console.log("Default, no entra");
-	break;
+    case 12:
+        console.log("Es 12");
+        break;
+    default:
+        console.log("Default, no entra");
+        break;
 }
 
 var strLetraA = "a";
 switch (strLetraA.toUpperCase()) {
-case "A":
-	console.log("Es A");
-	break;
-default:
-	console.log("Default, no entra");
-	break;
+    case "A":
+        console.log("Es A");
+        break;
+    default:
+        console.log("Default, no entra");
+        break;
 }
 
 console.log("Sentencia - while ");
 
 var i = 0;
 while (i < numerico) {
-	console.log(i);
-	i++;
+    console.log(i);
+    i++;
 }
 i = 0;
 do {
-	console.log(i);
-	i++;
+    console.log(i);
+    i++;
 } while (i < numerico);
 
 console.log("Sentencia - for ");
 
 for (var i = 0; i < numerico; i++) {
-	console.log(i);
+    console.log(i);
 }
 
 console.log("Sentencia - for loop ");
 
-var arreglo = [ "uno", "dos", "tres" ];
+var arreglo = ["uno", "dos", "tres"];
 for (var i in arreglo) {
-	console.log(arreglo[i]);
+    console.log(arreglo[i]);
 }
 
 
@@ -313,11 +310,11 @@ for (var i in arreglo) {
 console.log("\n\n Objetos simples y arreglos");
 
 var primerObjeto = {
-	fechaActual : Date(),
-	label : "primerObjeto",
-	getFechaActual : function() {
-		return "Este es la fecha actual" + this.fechaActual;
-	}
+    fechaActual: Date(),
+    label: "primerObjeto",
+    getFechaActual: function() {
+        return "Este es la fecha actual" + this.fechaActual;
+    }
 };
 
 console.log(primerObjeto.fechaActual);
@@ -340,9 +337,9 @@ console.log("Analizando valor null : " + primerObjeto.label);
  */
 
 var objetoYArreglos = {
-	arreglo : [ "arreglo 1", "arreglo 2", "arreglo 3" ],
-	"1" : "valor 1",
-	"2" : "valor 2"
+    arreglo: ["arreglo 1", "arreglo 2", "arreglo 3"],
+    "1": "valor 1",
+    "2": "valor 2"
 }
 
 console.log(objetoYArreglos[1]);
@@ -363,19 +360,19 @@ console.log(objetoYArreglos.arreglo["1"]);
 // El patron mas simple para la creacion de clases es utilizando una funcion de construccion de objeto
 console.log("\n\Factory Pattern");
 var functionCrearObjeto = function(nombre, edad, estado) {
-	var result = {
-		nombre : nombre,
-		edad : edad
-	};
+    var result = {
+        nombre: nombre,
+        edad: edad
+    };
 
-	result.estado = estado;
+    result.estado = estado;
 
-	result.log = function() {
-		// Notar el uso de this dentro de la funcion, referencia a una instancia de result
-		console.log(JSON.stringify(this));
-	}
+    result.log = function() {
+        // Notar el uso de this dentro de la funcion, referencia a una instancia de result
+        console.log(JSON.stringify(this));
+    }
 
-	return result;
+    return result;
 }
 
 var objetoFuncion1 = functionCrearObjeto("Nestor", 40, true);
@@ -386,15 +383,15 @@ objetoFuncion2.log();
 //Construction Pattern
 console.log("\n\nConstruction Pattern");
 var constructorCrearObjeto = function(nombre, edad, estado) {
-	this.nombre = nombre;
-	this.edad = edad;
-	this.estado = estado;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.estado = estado;
 
-	// El problema es que esta definicion de funcion se replica por cada uno
-	// de los objetos que creamos
-	this.log = function() {
-		console.log(JSON.stringify(this));
-	}
+    // El problema es que esta definicion de funcion se replica por cada uno
+    // de los objetos que creamos
+    this.log = function() {
+        console.log(JSON.stringify(this));
+    }
 }
 
 var constructorPersona1 = new constructorCrearObjeto("Nestor", 40, true);
@@ -405,12 +402,12 @@ constructorPersona2.log();
 //Prototype Construction Function
 console.log("\n\nPrototype Construction Function");
 var prototypeCrearObjeto = function(nombre, edad, estado) {
-	this.nombre = nombre;
-	this.edad = edad;
-	this.estado = estado;
+    this.nombre = nombre;
+    this.edad = edad;
+    this.estado = estado;
 }
 prototypeCrearObjeto.prototype.log = function() {
-	console.log(JSON.stringify(this));
+    console.log(JSON.stringify(this));
 }
 
 var prototypePersona1 = new prototypeCrearObjeto("Nestor", 40, true);
@@ -423,53 +420,52 @@ console.log("\n\n Callback Pattern");
 // Callback Functions - High order functions
 // En javascript las funciones son first class objects, se pueden pasar como argumentos.
 var funcionPrincipal = function(callback) {
-	console.log("Ejecutando el proceso principal");
-	callback();
+    console.log("Ejecutando el proceso principal");
+    callback();
 }
 
 funcionPrincipal(function() {
-	console.log("Callback");
+    console.log("Callback");
 });
 
 console.log("\n\n Strategy Pattern");
 //En javascript las funciones son first class objects, se pueden pasar como argumentos.
 function sum(a, b) {
-	return a + b;
-}
-;
+    return a + b;
+};
+
 function multiply(a, b) {
-	return a * b;
-}
-;
+    return a * b;
+};
 
 function calc(a, b, func) {
-	if (typeof func === "function")
-		return func(a, b);
+    if (typeof func === "function")
+        return func(a, b);
 }
 
 console.log(calc(2, 3, sum));
 console.log(calc(2, 3, multiply));
 console.log(calc(2, 3, (a, b) => {
-	return a + " y " + b;
+    return a + " y " + b;
 }));
 
 //En javascript las promesas son similares se compromete a hacer una tarea y resulta ok o falla
 // http://javascriptplayground.com/blog/2015/02/promises/
 console.log("\n\n Promises");
 var promesaCasamiento = new Promise(function(resolve, reject) {
-	console.log("Intentando casarse... esto va a llevar mucho tiempo...");
+    console.log("Intentando casarse... esto va a llevar mucho tiempo...");
 
-	var seCaso = Math.random() >= 0.5;
+    var seCaso = Math.random() >= 0.5;
 
-	if (seCaso) {
-		resolve("Todo bien");
-	} else {
-		reject("Todo mal");
-	}
+    if (seCaso) {
+        resolve("Todo bien");
+    } else {
+        reject("Todo mal");
+    }
 });
 
 promesaCasamiento.then((result) => {
-	console.log("Se caso " + result);
+    console.log("Se caso " + result);
 }).catch((reason) => {
-	console.log(" No se caso " + reason);
+    console.log(" No se caso " + reason);
 });
